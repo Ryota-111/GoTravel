04dfddb43b5dac74bc02532a09ce1785611e8a58
Ryota-111 <taismryotasis@gmail.com>
2025-09-22 12:21:13 +0900

    Initial Commit

---
+++ GoTravel/GoTravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoTravelApp.swift
+//  GoTravel
+//
+//  Created by Ryota Fujitsuka on 2025/09/22.
+//
+
+import SwiftUI
+
+@main
+struct GoTravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
